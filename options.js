a8f0686d71d59bfb93576a41aaf46f8a7eccaa60
phraznikov <phraznikov@gmail.com>
2014-03-17 15:39:27 -0700

    handle wacky user input

--- options.js
+++ options.js
@@ -1,7 +1,11 @@
 function save_options() {
 	var select = document.getElementById("wage");
-	localStorage.wage = select.value;
-	alert("Settings saved.");
+	if(isNaN(parseFloat(select.value))) {
+		alert("What are they paying you in, Trident Layers? \nTry formatting your wage without the currency symbol, like 10.50");
+	} else {
+		localStorage.wage = select.value;
+		alert("Settings saved.");
+	}
 }
 
 document.querySelector("#save").addEventListener("click", save_options);
