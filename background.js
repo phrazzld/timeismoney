04a4051fba2a63541445fa06eb9c5f9a6ad2d654
Phaedrus <phraznikov@gmail.com>
2014-09-07 21:26:45 -0700

    open options.html on install

--- background.js
+++ background.js
@@ -19,6 +19,14 @@ if(jQuery){
 } else { }
 
 
+function install_notice() {
+    if (localStorage.getItem('install_time'))
+        return;
 
+    var now = new Date().getTime();
+    localStorage.setItem('install_time', now);
+    chrome.tabs.create({url: "options.html"});
+}
+install_notice();
 
 
