49d0bea7468b0548d738283c367fa48d801ae6fe
Phaedrus <phraznikov@gmail.com>
2017-02-05 19:50:25 -0800

    Trim the fat... with a battleaxe

---
+++ content.js
@@ -0,0 +1,71 @@
+console.log("content.js loaded");
+walk(document.body);
+
+function walk(node) {
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
+
+  var child, next;
+
+  switch(node.nodeType) {
+    case 1:
+    case 9:
+    case 11:
+      child = node.firstChild;
+      while(child) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+    case 3:
+      plumb(node);
+      break;
+    default:
+      break;
+  }
+}
+
+function plumb(textNode) {
+  chrome.storage.sync.get(null, function(items) {
+    // Get saved user preferences
+    var frequency, amount, workingwage;
+    frequency = items["frequency"];
+    amount = items["amount"];
+    // Adjust saved income down to hourly
+    if(frequency == "yearly") {
+      workingwage = parseFloat(amount)/52/40;
+    } else if(frequency == "hourly") {
+      workingwage = parseFloat(amount);
+    }
+
+    var original = textNode.nodeValue;
+    var keeptruckin, newstring;
+    if(currency == "eur") {
+      keeptruckin = original.match(/(\€|EUR)(\s?)[0-9](([0-9]|\,)*\.?[0-9]{2}?)?/g);
+    } else if(currency == "gbp") {
+      keeptruckin = original.match(/(\£|GBP)(\s?)[0-9](([0-9]|\,)*\.?[0-9]{2}?)?/g);
+    } else {
+      keeptruckin = original.match(/(\$|USD)(\s?)[0-9](([0-9]|\,)*\.?[0-9]{2}?)?/g);
+    }
+    if(keeptruckin) {
+      newstring = original.trim();
+      newstring = newstring.replace(/[^\d.]/g, '');
+      var time = parseFloat(newstring) / workingwage;
+      var hours, minutes, msg;
+      if(!isNaN(time)) {
+        hours = Math.floor(time);
+        console.log("hours is " + hours);
+        minutes = Math.ceil(60 * (time - hours));
+        console.log("minutes is " + minutes);
+        if(minutes == 60) {
+          hours += 1;
+          minutes = 0;
+        }
+        msg = original + " (" + hours + "h " + minutes + "m" + ")";
+        console.log("msg is " + msg);
+        textNode.nodeValue = msg;
+      }
+    }
+  });
+}
---
+++ options.js
@@ -0,0 +1,53 @@
+function saveOptions() {
+  var currency = document.getElementById('currency').value;
+  var frequency = document.getElementById('frequency').value;
+  var amount = document.getElementById('amount').value;
+  amount = amount.replace(/(\$|,|€|£| +?)/g, '');
+  if(amount == "") {
+    amount = 0;
+  }
+  var status = document.getElementById('status');
+  if(isNaN(amount)) {
+    status.textContent = 'Error! Invalid amount entered.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 1000);
+  } else {
+    chrome.storage.sync.set({
+      currency: currency,
+      frequency: frequency,
+      amount: amount
+    }, function() {
+      status.textContent = 'Options saved.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 750);
+    });
+  }
+}
+
+function initializeOptions() {
+  chrome.storage.sync.get(null, function(items) {
+    var currency, frequency, amount;
+    currency = items['currency'];
+    frequency = items['frequency'];
+    amount = items['amount'];
+    if(currency !== undefined && currency !== null) {
+      document.getElementById('currency').value = currency;
+    }
+    if(frequency !== undefined && frequency !== null) {
+      document.getElementById('frequency').value = frequency;
+    }
+    if(amount !== undefined && amount !== null) {
+      document.getElementById('amount').value = numberWithCommas(amount);
+    }
+  });
+}
+
+// Code from http://bit.ly/1ooHsSO
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+document.addEventListener('DOMContentLoaded', initializeOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
