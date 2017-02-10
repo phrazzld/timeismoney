a0f370686851a6bd55101499053ed48060e76d2e
Phaedrus <phraznikov@gmail.com>
2017-02-10 15:33:15 -0800

    innerHTML is bad, mmkay?

--- content.js
+++ content.js
@@ -1,15 +1,41 @@
-chrome.storage.sync.get(null, function(items) {
-  if (items["disabled"] !== true) { 
-    var bod = document.body;
+chrome.storage.sync.get(null, function(obj) {
+  if (obj["disabled"] !== true) { 
+    walk(document.body);
+  }
+});
+
+// Credit to t-j-crowder on StackOverflow for this walk function
+// http://bit.ly/1o47R7V
+function walk(node) {
+  var child, next;
+
+  switch (node.nodeType) {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while (child) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+    case 3:  // Text node
+      convert(node);
+      break;
+  }
+}
+
+function convert(textNode) {
+  chrome.storage.sync.get(null, function(items) {
     var currencySymbol = items["currencySymbol"];
     var currencyLetters = items["currencyLetters"];
     var amount = items["amount"];
     var frequency = items["frequency"];
     var sourceMoney, workingWage, matchPattern;
-
     // Build matchPattern
     matchPattern = new RegExp('(\\' + currencySymbol + '|' + currencyLetters + ')\\x20?\\d(\\d|\\,)*(\\.\\d\\d)?', 'g');
-    bod.innerHTML = bod.innerHTML.replace(matchPattern, function(e) {
+    textNode.nodeValue = textNode.nodeValue.replace(matchPattern, function(e) {
       sourceMoney = parseFloat(e.replace(/[^\d.]/g, '')).toFixed(2);
       workingWage = parseFloat(amount);
       if (frequency == "yearly") {
@@ -18,8 +44,8 @@ chrome.storage.sync.get(null, function(items) {
       workingWage = workingWage.toFixed(2);
       return makeSnippet(e, workingWage);
     });
-  }
-});
+  });
+}
 
 function makeSnippet(sourceElement, workingWage) {
   var sourceMoney = parseFloat(sourceElement.replace(/[^\d.]/g, '')).toFixed(2);
